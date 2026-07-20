18b2bd715edeae8ec2d07a304d7aad84121b6c51
Gendalf4ever <chelovek438@gmail.com>
2026-07-20 21:12:59 +0300

    minor changes

--- lib/logic/filePicker.dart
+++ lib/logic/filePicker.dart
@@ -1,26 +1,23 @@
 import 'package:file_picker/file_picker.dart';
 
 class Filepicker {
-  //txt selection
-
+  // txt selection
   Future<String?> pickTxtFile() async { 
     FilePickerResult? result = await FilePicker.platform.pickFiles(
       type: FileType.custom,
       allowedExtensions: ['txt'],
     );
-    if (result != null && result.files.single.path != null){
+    if (result != null && result.files.single.path != null) {
       return result.files.single.path;
     }
-    return result?.files.single.path;
+    return null;
   }
 
-  //folder selection
-  Future<String?> pickImagesFolder() async{
+  // folder selection
+  Future<String?> pickImagesFolder() async {
     String? selectedDirectory = await FilePicker.platform.getDirectoryPath();
-
-    if (selectedDirectory != null){
+    if (selectedDirectory != null) {
       return selectedDirectory;
-
     }
     return null;
   }
